f477140c79841458a8a80836a4487e574a4e09a4
SamuelReinaldoRamirez <samuel.reinaldo.ramirez@gmail.com>
2020-12-23 01:07:41 +0100

    IN build your user interface whith polaris POST set up your app navigation PRE add the title bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,34 @@
 import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/dist/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
 import React from 'react';
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props;
-    return (
+    const { Component, pageProps, shopOrigin } = this.props;   
+    const config = { apiKey: API_KEY, shopOrigin, forceRedirect: true };
+     return (
       <React.Fragment>
         <Head>
           <title>Sample App</title>
           <meta charSet="utf-8" />
         </Head>
+        <Provider config={config}>
         <AppProvider i18n={translations}>            
         <Component {...pageProps} />
         </AppProvider>
+        </Provider>
       </React.Fragment>
     );
+  }
+}
+
+MyApp.getInitialProps = async ({ ctx }) => {
+  return {
+    shopOrigin: ctx.query.shop,
   }
 }
 
